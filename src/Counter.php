aca7ae92aec266616f822a5d6ead6d7d179703d3
alyouma <alyoumaakmal4@gmail.com>
2025-04-23 13:47:18 +0700

    unit test

---
+++ src/Counter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Alyou\BelajarPhpUnitTest2;
+
+class Counter
+{
+
+    private int $counter = 0;
+
+
+    public function increment(): void
+    {
+        $this->counter++;
+    }
+
+    public function getCounter(): int
+    {
+        return $this->counter;
+    }
+}
---
+++ tests/CounterTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Alyou\BelajarPhpUnitTest2;
+
+use PHPUnit\Framework\TestCase;
+
+class CounterTest extends TestCase
+{
+
+
+    public function testCounter()
+    {
+        $counter = new Counter();
+        $counter->increment();
+        echo $counter->getCounter() . PHP_EOL;
+    }
+}
